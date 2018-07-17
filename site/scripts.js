d91cc402fa88a921069f1b48165c4c454f1ff9dd
Block Bros <41334731+BlockBros@users.noreply.github.com>
2018-07-17 22:26:24 +0200

    Updating

--- site/scripts.js
+++ site/scripts.js
@@ -2,12 +2,44 @@ $(document).ready(function() {
 	windowResize();
 	
 	$.getJSON("site/site.json").done(function(json) {
-		document.title = pagename + " | " + json["website-name"];
+		document.title = json["website-name"] + " | " + pagename;
 		$("#pagename").html(pagename);
 		$("#footer").html(json["footer"]);
 		
+		/* Read Navigation data */
+		var navigation = "<div class='nav-item separate open-navigation'><i class='fas fa-bars'></i></div>";
+		for(var i = 0; i < json["navigation"].length; i++) {
+			navigation = navigation + "<a href='" + json["navigation"][i][0] + "'><div class='nav-item'>" + json["navigation"][i][1] + "</div></a>";
+		}
+		$(".navigation").html(navigation);
+		
+		/* Read Side-Navigation data */
+		var side_navigation = "<div class='nav-item close'><i class='fas fa-times'></i></div>";
+		for(var i = 0; i < json["side-navigation"].length; i++) {
+			side_navigation = side_navigation + "<a href='" + json["side-navigation"][i][0] + "'><div class='nav-item'>" + json["side-navigation"][i][1] + "</div></a>";
+		}
+		$(".side-navigation").html(side_navigation);
+		
+		/* Read Header data */
+		var header_path = "<a href='" + json["home-url"] + "'><i class='fas fa-home'></i></a><span>/</span>";
+		var header = page_header.split("/");
+		for(var i = 0; i < header.length; i++) {
+			var index = null;
+			for(var j = 0; j < json["header"].length; j++) {
+				if(json["header"][j][1] == header[i]) {
+					index = j;
+				}
+			}
+			if(index !== null) {
+				header_path = header_path + "<a href='" + json["header"][index][0] + "'>" + json["header"][index][1] + "</a><span>/</span>";
+			}
+		}
+		header_path = header_path + "<a href='#'>" + pagename + "</a>";
+		$(".header").html(header_path);
+		
+		/* Read Footer-Links data */
 		var footer_links = "";
-		for(var i = 0; i < (count(json["footer-links"]) - 1); i++) {
+		for(var i = 0; i < json["footer-links"].length; i++) {
 			footer_links = footer_links + "<a href='" + json["footer-links"][i][0] + "'>" + json["footer-links"][i][1] + "</a>";
 		}
 		$(".footer-links").html(footer_links);
@@ -20,7 +52,7 @@ $(document).ready(function() {
 		navigation.toggleClass("dropped");
 	});
 	
-	$(".side-navigation .close").click(function() {
+	$(document).on("click", ".side-navigation .close", function() {
 		var navigation = $(".side-navigation");
 		
 		navigation.fadeOut(200);
